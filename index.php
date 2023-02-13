f583438e38f961987bf6f6d162286b77cefa5223
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-13 12:50:38 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title> PHP </title>
+    </head>
+    <body>
+
+ <h1>My First PHP Page</h1>
+
+<?php
+$var1=10;
+
+echo "Value of var1 is ".$var1;
+echo "<br/>";
+
+$var1="Wiztoonz";
+
+echo "Value of var1 is $var1<br/>";
+
+$var1=3.142;
+
+echo "Value of var1 is $var1";
+?>
+
+
+
+    </body>
+</html>
