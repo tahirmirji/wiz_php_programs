5985391433fc61e7e9cd4804f177f510ac042cf4
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-14 12:29:05 +0530

    Assoc

--- array.php
+++ array.php
@@ -1,7 +1,12 @@
 <?php
-$num = array(1,2,3,4,5);
-foreach($num as $singlevalue)
+    //  =    0,1,2,3,4
+// $num = array(1,2,3,4,5);
+
+$salaries = array("A"=>"Wiztoonz","B"=>200);
+foreach($salaries as $singlevalue)
 {
     echo " value is $singlevalue <br/>";
 }
+
+echo $salaries['B'];
 ?>
