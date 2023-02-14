7685566e2945bedfd9c5178422b3d8fe8bdbae67
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-14 12:12:29 +0530

    e

--- var.php
+++ var.php
@@ -1,13 +1,46 @@
 <?php
-$variable = "name";
 
-$litrally= 'My $variable will not print';
+// $Result = 2+4/2*4-(6-2); // BODMAS rule
+    //  = 2+4/2*4-4
+    //  = 2+2*4-4
+    //  = 2+8-4
+    //  = 10-4
+    //  = 6
+// echo $Result;
 
-print($litrally);
+// **********************
 
-print "<br/>";
 
-$litrally ="My $variable will print";
 
-print($litrally);
+function Afunction(&$wiz)
+{
+    $wiz =10;
+}
+
+$outsideVar = 5;
+print("Before".$outsideVar);
+
+Afunction($outsideVar);
+print("<br/>");
+print("After".$outsideVar);
+
+
+
+
+
+
+
+
+
+// $variable = "name";
+
+// $litrally= 'My $variable will not print';
+
+// print($litrally);
+
+// print "<br/>";
+
+// $litrally ="My $variable will print";
+
+// print($litrally);
 ?>
