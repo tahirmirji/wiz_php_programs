228e6e2994a5f822260f0b6808b545733727ca4f
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-14 12:36:08 +0530

    Array

--- array.php
+++ array.php
@@ -2,11 +2,11 @@
     //  =    0,1,2,3,4
 // $num = array(1,2,3,4,5);
 
-$salaries = array("A"=>"Wiztoonz","B"=>200);
+$salaries = array("A"=> array("C"=> array("E"=>500),"D"=>400),"B"=>200);
 foreach($salaries as $singlevalue)
 {
     echo " value is $singlevalue <br/>";
 }
 
-echo $salaries['B'];
+echo $salaries['A']["C"]["E"];
 ?>
