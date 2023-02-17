dc80701412428b5d92c47d2c6d92eab83bfe4c79
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-17 12:39:50 +0530

    17-02

--- contact.php
+++ contact.php
@@ -2,9 +2,11 @@
 
     if(isset($_POST['Submit']))
     {
-        if(isset($_POST['txt1']))
+        if(isset($_POST['txt1']) && isset($_POST['txt2']) && isset($_POST['course']))
         {
             echo "Hi, ".$_POST['txt1'];
+            echo ", Your email is ".$_POST['txt2'];
+            echo " and contact number is ".$_POST['txt3'];
         }
 
     }
