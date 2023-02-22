0829489d3157040dcaa8e61cd9d87fc28b8ec102
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-22 12:48:53 +0530

    php

--- connect.php
+++ connect.php
@@ -2,17 +2,30 @@
 $db_host ='localhost';
 $db_user = 'root';
 $db_pass = '';
-$db_name = 'library';
+$db_name = 'wiz';
 
 $connection = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 
 if($connection != null)
 {
-    echo "Mysql Database connection success";
+    echo "Mysql Database <b>connection success</b>";
+    
+    $query = "create table employee4( empid int(10) not null auto_increment, empname varchar(40) not null, empemail varchar(40) not null, primary key(empid) )";
 
+    $result = mysqli_query($connection, $query);
+
+    if( $result > 0 )
+    {
+        echo "<h2> Code: $result, Table Created </h2>";
+        
+    }
+    else
+    {
+        echo "<h2> Code: $result, Table Creation failed </h2>";
+    }
 }
 else
 {
-    echo "Database connection failed.";
+    die ("Database connection failed.");
 }
 ?>
--- contact.php
+++ contact.php
@@ -1,13 +1,41 @@
 <?php
 
-    if(isset($_POST['Submit']))
-    {
-        if(isset($_POST['txt1']) && isset($_POST['txt2']) && isset($_POST['course']))
-        {
-            echo "Hi, ".$_POST['txt1'];
-            echo ", Your email is ".$_POST['txt2'];
-            echo " and contact number is ".$_POST['txt3'];
-        }
+if(isset($_POST['Submit']))
+{
 
+    if(  isset(  $_POST['txt1']   )    &&      isset(    $_POST['txt2']   )   )
+    {
+
+        $empname = $_POST['txt1'];
+        $empemail   = $_POST['txt2'];
+        
+    
+$host ='localhost';
+$username ='root';
+$pass ='';
+$dbname='wiz';
+
+$connection = mysqli_connect($host,$username,$pass,$dbname);
+
+if($connection != null)
+{
+    echo 'Connection Success........<br>';
+    $query = " INSERT INTO employee values(NULL,'".$empname."' , '".$empemail."')";
+    $reply = mysqli_query($connection,$query);
+
+    if($reply>0)
+    {
+        echo "New Record added to table";
     }
+    else
+    {
+        echo "Failed to add new record";
+    }
+
+}
+
+    }    
+}      
+
+
 ?>
